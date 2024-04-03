52611948071401b2caecd34361a25c5ca002908c
Kimon Papahadjopoulos <kimonp@gmail.com>
2024-04-03 19:12:00 -0400

    use formal api for tests

--- src/dot_examples.rs
+++ src/dot_examples.rs
@@ -3,17 +3,26 @@
 use crate::graph::Graph;
 
 pub const DOT_EXAMPLES: [(&str, &str); 17] = [
-    ("a_to_b_and_c",        "digraph {a -> b; a -> c;}"),
-    ("b_and_c_to_a",        "digraph {b -> a; c -> a;}"),
-    ("a_to_c_and_b_to_a",   "digraph {a -> c; b -> a;}"),
-    ("two_acyclic",         "digraph {a -> b; b -> a;}"),
-    ("three_acyclic",       "digraph {a -> b; b -> c; c -> a; }"),
-    ("complex_acyclic",     "digraph { a -> c; b -> d; c -> e; e -> d; d -> c; }"),
-    ("flux_capacitor",      "digraph {a -> c; b -> c; c -> d}"),
-    ("t1_2_1",              "digraph { a -> b; a -> c; b -> d; c -> d; }"),
-    ("a_to_4_nodes",        "digraph {a -> b; a -> c; a -> d; a -> e;}"),
-    ( "simple_scramble",    "digraph { a -> b; a -> c; c -> d; b -> e; }"),
-    ( "reverse_scramble",   "digraph { a -> b; a -> c; b -> e; c -> d; }"),
+    ("a_to_b_and_c", "digraph {a -> b; a -> c;}"),
+    ("b_and_c_to_a", "digraph {b -> a; c -> a;}"),
+    ("a_to_c_and_b_to_a", "digraph {a -> c; b -> a;}"),
+    ("two_acyclic", "digraph {a -> b; b -> a;}"),
+    ("three_acyclic", "digraph {a -> b; b -> c; c -> a; }"),
+    (
+        "complex_acyclic",
+        "digraph { a -> c; b -> d; c -> e; e -> d; d -> c; }",
+    ),
+    ("flux_capacitor", "digraph {a -> c; b -> c; c -> d}"),
+    ("t1_2_1", "digraph { a -> b; a -> c; b -> d; c -> d; }"),
+    ("a_to_4_nodes", "digraph {a -> b; a -> c; a -> d; a -> e;}"),
+    (
+        "simple_scramble",
+        "digraph { a -> b; a -> c; c -> d; b -> e; }",
+    ),
+    (
+        "reverse_scramble",
+        "digraph { a -> b; a -> c; b -> e; c -> d; }",
+    ),
     (
         "tse_paper_example_2_3",
         "digraph {
@@ -96,11 +105,17 @@ pub const DOT_EXAMPLES: [(&str, &str); 17] = [
     ),
 ];
 
-pub fn dot_example_graph(title: &str) -> Graph {
+pub fn dot_example_str(title: &str) -> &str {
     for (dot_title, dot) in DOT_EXAMPLES {
         if title == dot_title {
-            return Graph::from(dot)
+            return dot;
         }
     }
     panic!("Could not find requested example: {title}")
+}
+
+pub fn dot_example_graph(title: &str) -> Graph {
+    let dot = dot_example_str(title);
+
+    Graph::from(dot)
 }
